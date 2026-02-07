fb001c88ab9763fef5f08f12f06357fbef2693f1
anna-ravenart <nastena.voronenko131@gmail.com>
2026-02-07 17:00:05 +0200

    finished

---
+++ rock_paper_scissors_game.py
@@ -0,0 +1,41 @@
+# Rock, Paper, Scissors game
+
+import random
+
+MOVES = ("rock", "paper", "scissors")
+human_move = ""
+computer_move = ""
+
+
+while True:
+    print("\nLet's play Rock, Paper, Scissors!\n")
+
+    # computer's move by random
+    computer_move = random.choice(MOVES)
+
+    # player's move
+    while human_move not in MOVES:
+        print("Choose your move: rock, paper, or scissors..")
+        human_move = input("\nEnter your choice: ").lower()
+
+    print("\nYou chose: ", human_move, "\nAnd I chose: ", computer_move)
+
+    # check for a tie and check for a winner
+    if human_move == computer_move:
+        print("\n\nIt's a tie!")
+    elif human_move == "rock" and computer_move == "scissors":
+        print("\nYou win this round! Congratulations!\n")
+    elif human_move == "scissors" and computer_move == "paper":
+        print("\nYou win this round! Congratulations!\n")
+    elif human_move == "paper" and computer_move == "rock":
+        print("\nYou win this round! Congratulations!\n")
+    else:
+        print("\nComputer wins this round!\n")
+
+
+    human_move = ""
+    answer = input("Do you want play again? (y/n): ").lower()
+    if answer == "no":
+        print("\nThanks for playing! Bye!")
+        input("\nPress Enter to exit")
+        break
